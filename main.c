0e38f6c2c29862581dda34cb19267cd8214d6958
Thomas Midek <38216060+ThomasMidek@users.noreply.github.com>
2018-05-07 13:19:02 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,99 +1,208 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
 
-#define MSG_LENG 1
+#define MSG_LEN 2
+#define BUF_LEN 2
+#define RAND 0.2
 
-/*pipe-creation*/
-int pipe1[2], pipe2[2];
-
-/*sigaction struct creation*/
 struct sigaction actioninitiator = {0};
 struct sigaction actionresponder = {0};
+struct sigaction actiontimer = {0};
 
-void abp_initiator(){
-    char msg_buf[1];
-    if(read(pipe2[0],msg_buf,MSG_LENG)==-1){
-        printf("agb_initiator error!");
-    }
-    else{
-        printf("%s",msg_buf);
-    }
+int pipe1[2], pipe2[2];
+int signummer;
+int retransmissionCounter = 0;
+
+void abp_initiator(int signr){
+    signummer = signr;
+    char msg_buf[BUF_LEN];
+    read(pipe2[0],&msg_buf,MSG_LEN);
+    alarm(0);
+    printf("Initiator erhält: %c | %c\n\n",msg_buf[0], msg_buf[1]);
 }
 
 void abp_responder(){
-    char msg_buf[1];
-    if(read(pipe1[0],msg_buf,MSG_LENG)==-1){
-        printf("agb_responder error!");
-    }
-    else{
-        printf("%c",msg_buf);
-    }
+    char msg_buf[BUF_LEN];
+    read(pipe1[0],&msg_buf,MSG_LEN);
+    printf("Responder erhält: %c | %c\n\n",msg_buf[0],msg_buf[1]);
 }
 
-void initiator(pid_t r_pid){
-    char msg_buf[1]= "a";
-    switch (r_pid) {
-
-        case -1:    //error-handling
-            printf("Error!");
-            break;
-
-        case 0:     //child-process
-            printf("child");
-            close(pipe1[1]);
-            close(pipe2[0]);
-            actionresponder.sa_handler = abp_responder;
-            sigaction(SIGUSR1,&actionresponder,0);
-            msg_buf[0]='x';
-            pid_t ppid= getppid();
-            while(1==1){
-                pause();
-                write(pipe2[1],msg_buf,MSG_LENG);
-                kill(ppid,SIGUSR2);
-            }
-            sleep(4);
-            close(pipe1[0]);
-            close(pipe2[1]);
-            exit(0);
-
-        default:    //parent-process
-            printf("parent");
-            close(pipe1[0]);
-            close(pipe2[1]);
-            actioninitiator.sa_handler = abp_initiator;
-            sigaction(SIGUSR2,&actioninitiator,0);
-            msg_buf[0]='y';
-            sleep(2);
-            write(pipe1[1],msg_buf,MSG_LENG);
-            kill(r_pid,SIGUSR1);
-            while(1==1){
-                printf("test");
-                pause();
-                write(pipe1[1],msg_buf,MSG_LENG);
-                kill(r_pid,SIGUSR1);
-            }
-            sleep(4);
-            close(pipe1[1]);
-            close(pipe2[0]);
-            exit(0);
-    }
+void abp_timer(int signr){
+    signummer = signr;
+    retransmissionCounter++;
+    printf("An Error has occured! Retransmitting...\n\n");
 }
 
-int main(void) {
+void send(int pipeAdress,char control, char message){
+    char msg_buf[BUF_LEN];
+    msg_buf[0] = message;
+    msg_buf[1] = control;
+    write(pipeAdress,&msg_buf,MSG_LEN);
+}
 
-    char parentMessage[] = "TheQuickBrownFoxOle";
-    char childMessage[] = "JumpsOverTheLazyDog";
+int main() {
+    //both text segments
+    char send_msg[] = ("TheQuickBrownFoxOle");
+    char respond_msg[] = ("JumpsOverTheLazyDog");
 
-    if(pipe(pipe1)==-1||pipe(pipe2)==-1){
-        printf("Pipe-Error!");
-        return -1;
+    //countervariables
+    int i;
+    int state;
+
+    //pipes
+    pipe(pipe1);
+    pipe(pipe2);
+
+    //random number generator
+    srand(time(NULL));
+
+    //process id for parent/child determination
+    pid_t r_pid;
+
+    if((r_pid = fork())<0){
+        //error handling
+        printf("Error!");
+        return 1;
+    }
+    else if(r_pid>0){
+        //parent process coding
+        i = 0;
+        state = 0;
+
+        char msg_buffer[MSG_LEN];
+
+        close(pipe1[0]);
+        close(pipe2[1]);
+
+        //sigaction
+        actioninitiator.sa_handler = abp_initiator;
+        sigaction(SIGUSR2, &actioninitiator, 0);
+        actiontimer.sa_handler = abp_timer;
+        sigaction(SIGALRM, &actiontimer, 0);
+
+        sleep(2);
+        printf("Starting transmission...\n\n");
+        while(i<strlen(send_msg)){
+            switch (state){
+                case 0:
+                    msg_buffer[0] =  send_msg[i];
+                    msg_buffer[1] = '0';
+                    if(((float)rand()/(float)RAND_MAX)>=RAND) {
+                        write(pipe1[1], &msg_buffer, MSG_LEN);
+                        alarm(2);
+                        kill(r_pid,SIGUSR1);
+                        i++;
+                    }
+                    else {
+                        alarm(2);
+                    }
+                    state = 1;
+                    break;
+                case 1:
+                    pause();
+                    switch(signummer){
+                        case SIGUSR2:
+                            msg_buffer[0] = send_msg[i];
+                            msg_buffer[1] = '1';
+                            if(((float)rand()/(float)RAND_MAX)>=RAND) {
+                                write(pipe1[1], &msg_buffer, MSG_LEN);
+                                alarm(2);
+                                kill(r_pid,SIGUSR1);
+                                i++;
+                            }
+                            else {
+                                alarm(2);
+                            }
+                            state = 2;
+                            break;
+                        case SIGALRM:
+                            msg_buffer[0] =  send_msg[i];
+                            msg_buffer[1] = '0';
+                            if(((float)rand()/(float)RAND_MAX)>=RAND) {
+                                write(pipe1[1], &msg_buffer, MSG_LEN);
+                                alarm(2);
+                                kill(r_pid,SIGUSR1);
+                                i++;
+                            }
+                            else{
+                                alarm(2);
+                            }
+                            break;
+                    }
+                    break;
+                case 2:
+                    pause();
+                    switch (signummer) {
+                        case SIGUSR2:
+                            msg_buffer[0] =  send_msg[i];
+                            msg_buffer[1] = '0';
+                            if(((float)rand()/(float)RAND_MAX)>=RAND) {
+                                write(pipe1[1], &msg_buffer, MSG_LEN);
+                                alarm(2);
+                                kill(r_pid,SIGUSR1);
+                                i++;
+                            }
+                            else {
+                                alarm(2);
+                            }
+                            state = 1;
+                            break;
+                        case SIGALRM:
+                            msg_buffer[0] = send_msg[i];
+                            msg_buffer[1] = '1';
+                            if(((float)rand()/(float)RAND_MAX)>=RAND) {
+                                write(pipe1[1], &msg_buffer, MSG_LEN);
+                                alarm(2);
+                                kill(r_pid,SIGUSR1);
+                                i++;
+                            }
+                            else {
+                                alarm(2);
+                            }
+                            break;
+                    }
+            }
+        }
+        sleep(2);
+        printf("Durchgeführte Retransmissionen: %d\n\n",retransmissionCounter);
+        close(pipe1[1]);
+        close(pipe2[0]);
+        exit(0);
     }
     else{
-        initiator(fork());
+        //child process coding
+        i = 0;
+        state = 1;
+        close(pipe1[1]);
+        close(pipe2[0]);
+        actionresponder.sa_handler = abp_responder;
+        sigaction(SIGUSR1, &actionresponder, 0);
+        while(i<strlen(respond_msg)){
+            pause();
+            switch(state){
+                case 1:
+                    send(pipe2[1], '0', respond_msg[i]);
+                    state = 2;
+                    i++;
+                    break;
+                case 2:
+                    send(pipe2[1], '1', respond_msg[i]);
+                    state = 1;
+                    i++;
+                    break;
+            }
+            kill(getppid(),SIGUSR2);
+        }
+        sleep(2);
+        close(pipe1[0]);
+        close(pipe2[1]);
+        exit(0);
     }
 }
